ce9994d7fc5a1493f3097cfd67aa859f1eabfb13
mamoo <m.pedriale@gmail.com>
2015-12-16 21:20:46 +0100

    Added comment to Day 8 js solution files.

--- day08/day8_1.js
+++ day08/day8_1.js
@@ -1,3 +1,7 @@
+/**
+ * Solution of Advent of Code, day 8 Challenge
+ * http://adventofcode.com/day/8
+ */
 var fs = require('fs');
 fs.readFile("input.txt", 'utf8', function(err, data) {
 	var lines = data.split('\n'),
--- day08/day8_2.js
+++ day08/day8_2.js
@@ -1,3 +1,8 @@
+/**
+ * Solution of Advent of Code, day 8 Challenge
+ * http://adventofcode.com/day/8
+ * Part 2
+ */
 var fs = require('fs');
 fs.readFile("input.txt", 'utf8', function(err, data) {
 	var lines = data.split('\n'),
