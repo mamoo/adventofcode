a0503b3f70d5c22fc4bfdc7bab95ba93e86739b1
mamoo <m.pedriale@gmail.com>
2015-12-16 19:03:27 +0100

    Added solution to day 9, challenge 2

---
+++ day9/day9_2.js
@@ -0,0 +1,23 @@
+var fs = require('fs'),
+	_ = require('lodash'),
+	Combinatorics = require('js-combinatorics'),
+	distances = {};
+
+function findDistance(array){
+	var dist = 0;
+	for(var i = 0; i < array.length - 1; i++){ dist = dist + distances[array[i]][array[i+1]]; }
+	return dist;
+}
+
+fs.readFile("input.txt", 'utf8', function(err, data) {
+	data.split('\n').forEach(function(edgeInfo){
+			var tokens = edgeInfo.split(' ');
+			if (!distances[tokens[0]]) distances[tokens[0]] = {};
+				distances[tokens[0]][tokens[2]] = parseInt(tokens[4]);
+			if (!distances[tokens[2]]) distances[tokens[2]] = {};
+				distances[tokens[2]][tokens[0]] = parseInt(tokens[4]);
+	});
+	var permutations = Combinatorics.permutation(_.uniq(Object.keys(distances)));
+	var distance = Math.max.apply(0, permutations.map(findDistance));
+	console.log('Max distance is ' + distance);
+});
