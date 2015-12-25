b6e480cb8d432fd8f639e941e6637544e3ad540b
mamoo <m.pedriale@gmail.com>
2015-12-25 20:30:40 +0100

    Added solution to day 18, challenge 1.

---
+++ day18/day18_1.js
@@ -0,0 +1,45 @@
+/**
+ * Answer to Advent of Code, day 18 Challenge
+ * http://adventofcode.com/day/18
+ */
+var fs = require('fs'),
+	maxItems = 100;
+
+function getOnNeighbours(rowIndex, colIndex, matrix) {
+	var count = 0;
+	for (var i = rowIndex - 1; i < rowIndex + 2; i++)
+		for (var j = colIndex - 1; j < colIndex + 2; j++)
+			if (i >= 0 && i < maxItems
+				&& j >= 0 && j < maxItems
+				&& (i !== rowIndex || j !== colIndex) 
+				&& matrix[i][j]) count++;
+	return count;
+}
+
+function animate(matrix, currentIndex){	
+	var newMatrix = matrix.map(function(currentLine,rowIndex){
+		return currentLine.map(function(currentLight, colIndex){
+			if (currentLight)
+				return getOnNeighbours(rowIndex, colIndex, matrix) === 2 
+					   || getOnNeighbours(rowIndex, colIndex, matrix) === 3;
+			else 
+				return getOnNeighbours(rowIndex, colIndex, matrix) === 3;
+		});
+	});
+    if (currentIndex === maxItems) { 
+		console.log(newMatrix.reduce(function(previousValue, currentValue){
+			return previousValue + currentValue.reduce(function(prev, curr){ 
+				return prev + (curr ? 1 : 0); 
+			}, 0); 
+		}, 0));
+		return;
+	};
+	animate(newMatrix, ++currentIndex);
+}
+
+fs.readFile('input.txt', 'utf8', function(err, data) {
+	var lines = data.split('\n'),
+		matrix = []; 
+	matrix = lines.map(function(line){ return line.split('').map(function(status){ return status === '#' ? 1 : 0;}); });
+	animate(matrix, 1);
+});
